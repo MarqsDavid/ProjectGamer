c01bb39a0657fa731eb4043ffded42e88839bdc0
David Marques Mega de Sousa <88468918+MarqsDavid@users.noreply.github.com>
2022-08-15 21:56:10 -0300

    update

---
+++ js/file.js
@@ -0,0 +1,95 @@
+let container = document.querySelector("#container");
+let dino = document.querySelector("#mario");
+let block = document.querySelector("#block");
+let road = document.querySelector("#road");
+let cloud = document.querySelector(".cloud");
+let score = document.querySelector("#score");
+let gameOver = document.querySelector("#gameOver");
+let text1 = document.querySelector("#text1");
+
+
+//declaring variable for score
+let interval = null;
+let playerScore = 0;
+
+
+//function for score
+let scoreCounter = () => {
+    playerScore++;
+    score.innerHTML = `Score <b>${playerScore}</b>`;
+}
+
+
+//start Game
+
+const start = () => {
+
+    document.getElementById("text1").style.color = "rgb(236, 236, 236)";
+
+    block.classList.add('block-animation');
+
+    mario.src = './images/mario.gif';
+    mario.style.width = '150px';
+    mario.style.marginLeft = '50px';
+        
+
+    audioStart.play();
+}
+
+document.addEventListener('keydown', start);
+
+
+
+window.addEventListener("keydown", (start) => {
+    //    console.log(start);
+    if (start.code == "Space") {
+        gameOver.style.display = "none";
+        block.classList.add("blockActive");
+        road.firstElementChild.style.animation = "roadAnimate 1.5s linear infinite";
+        cloud.firstElementChild.style.animation = "cloudAnimate 2s linear infinite";
+            
+        //score
+        let playerScore = 0;
+        interval = setInterval(scoreCounter, 200);
+    }
+});
+
+
+//jump Your Character
+window.addEventListener("keydown", (e) => {
+    //    console.log(e);
+
+    if (e.key == "ArrowUp")
+        if (mario.classList != "marioActive") {
+            mario.classList.add("marioActive");
+
+            //                remove class after 0.5 seconds
+            setTimeout(() => {
+                mario.classList.remove("marioActive");
+            }, 500);
+        }
+});
+
+//'Game Over' if 'Character' hit The 'Block' 
+let result = setInterval(() => {
+    let marioBottom = parseInt(getComputedStyle(mario).getPropertyValue("bottom"));
+    //    console.log("dinoBottom" + dinoBottom);
+
+    let blockLeft = parseInt(getComputedStyle(block).getPropertyValue("left"));
+    //    console.log("BlockLeft" + blockLeft);
+
+    if (marioBottom <= 90 && blockLeft >= 20 && blockLeft <= 145) {
+        //        console.log("Game Over");
+
+
+             
+        gameOver.style.display = "block";
+        block.classList.remove("blockActive");
+        road.firstElementChild.style.animation = "none";
+        cloud.firstElementChild.style.animation = "none";
+        clearInterval(interval);
+        playerScore = 0;
+        
+    }
+
+}, 10);
